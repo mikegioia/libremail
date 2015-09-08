eaabc978ebff0f3fdc24b38229ddca472bf53af8
Mike Gioia <mikegioia@gmail.com>
2015-09-07 22:57:03 -0400

    Added stub create method for account model

--- sync/src/Models/Account.php
+++ sync/src/Models/Account.php
@@ -4,5 +4,12 @@ namespace App\Models;
 
 class Account extends \App\Model
 {
-    
+    /**
+     * Create a new account record.
+     * @param array $data
+     */
+    static function create( $data )
+    {
+        
+    }
 }
