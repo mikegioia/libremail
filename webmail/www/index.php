579509cc484239e9dc7a6e16d9257ad2bd95dfbc
Mike Gioia <mikegioia@gmail.com>
2017-03-27 07:01:46 -0400

    Added some UTF calls.

--- webmail/www/index.php
+++ webmail/www/index.php
@@ -3,6 +3,12 @@
 // Autoload application and vendor libraries
 require( __DIR__ .'/../vendor/autoload.php' );
 
+// Tell PHP that we'll be outputting UTF-8 to the browser
+mb_http_output( 'UTF-8' );
+// Tell PHP that we're using UTF-8 strings until the end
+// of the script
+mb_internal_encoding( 'UTF-8' );
+
 // Load constants and configuration
 
 // Set up route callbacks and trigger routing
