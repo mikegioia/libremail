9f0e0ab3ecee565a3c9eda8c24aca900f1d38c49
netandreus <netandreus@gmail.com>
2020-03-18 20:24:59 +0300

    - Add field "sync_failed_attempts" to table "folders" Re #31

--- sync/db/003_folders.sql
+++ sync/db/003_folders.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS `folders` (
   `name` varchar(150) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `count` int(10) unsigned DEFAULT '0',
   `synced` int(10) unsigned DEFAULT '0',
+  `sync_failed_attempts` INT(10) NULL DEFAULT '0',
   `uid_validity` int(10) unsigned DEFAULT '0',
   `deleted` tinyint(1) unsigned DEFAULT '0',
   `ignored` tinyint(1) unsigned DEFAULT '0',
