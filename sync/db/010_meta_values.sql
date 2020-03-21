445abd79614748ef1f05bb5aacf9847e9c062da4
Mike Gioia <mikegioia@gmail.com>
2020-03-21 15:36:28 -0700

    Formatting

--- sync/db/010_meta_values.sql
+++ sync/db/010_meta_values.sql
@@ -1,12 +1,12 @@
 INSERT INTO `meta`
-(`key`, `value`, `updated_at`)
+  (`key`, `value`, `updated_at`)
 VALUES
-('asleep', 0, NOW()),
-('running', 0, NOW()),
-('sync_pid', '', NOW()),
-('heartbeat', '', NOW()),
-('start_time', '', NOW()),
-('folder_stats', '', NOW()),
-('active_folder', '', NOW()),
-('active_account', '', NOW()),
-('last_sync_time', 0, NOW());
+  ('asleep', 0, NOW()),
+  ('running', 0, NOW()),
+  ('sync_pid', '', NOW()),
+  ('heartbeat', '', NOW()),
+  ('start_time', '', NOW()),
+  ('folder_stats', '', NOW()),
+  ('active_folder', '', NOW()),
+  ('active_account', '', NOW()),
+  ('last_sync_time', 0, NOW());
